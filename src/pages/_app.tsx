04f08d608d3b21260df623d49ad439ced814c3f2
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-04-15 16:43:40 +0800

    added providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
+import { NextUIProvider } from "@nextui-org/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider>
+      <ChakraProvider>
+        <NextUIProvider>
+          <Component {...pageProps} />{" "}
+        </NextUIProvider>
+      </ChakraProvider>
+    </MantineProvider>
+  );
 }
